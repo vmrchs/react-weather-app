c0c6169dfd9f877294ae5c69c60de275c12f4da1
vmrchs <vmrchs@gmail.com>
2023-03-23 15:24:29 -0300

    SETUP: Local variables

--- src/Api.jsx
+++ src/Api.jsx
@@ -1,12 +1,9 @@
 export const geoApiOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": `${JSON.stringify(import.meta.env.VITE_GEODB_API_KEY)}`,
+    "X-RapidAPI-Key": `${import.meta.env.VITE_GEODB_API_KEY}`,
     "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
   },
 };
 
-fetch("https://wft-geo-db.p.rapidapi.com/v1/geo/cities", options)
-  .then((response) => response.json())
-  .then((response) => console.log(response))
-  .catch((err) => console.error(err));
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,9 +1,18 @@
 import React, { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
+import { GEO_API_URL, geoApiOptions } from "../../Api";
 
 export default function Search({ onSearchChange }) {
   const [search, setSearch] = useState(null);
-  const loadOptions = (inputValue) => {};
+  const loadOptions = (inputValue) => {
+    return fetch(
+      `${GEO_API_URL}/cities?minPopulation=200000&namePrefix=${inputValue}`,
+      geoApiOptions
+    )
+      .then((response) => response.json())
+      .then((response) => console.log(response))
+      .catch((err) => console.error(err));
+  };
 
   const handleOnChange = (searchData) => {
     setSearch(searchData);
